4f60bec259bd48691f951eedd519f6fa9ca42fe8
satudoreimiruna <atudorei.miruna@yahoo.ro>
2024-01-09 18:50:43 +0200

    add user entity

---
+++ demo/src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,23 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String username;
+    private String password;
+    private String role;
+}
